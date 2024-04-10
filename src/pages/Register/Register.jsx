6b9f0981de9ee35bbf774eb34a2df28569b5fd1a
Shahadath Hossen Sajib <shahadathhossensajib732@gmail.com>
2024-04-10 12:41:50 +0600

    password show/hide toggle added on register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,10 +3,12 @@ import { Link, useNavigate, useLocation } from "react-router-dom";
 import { AuthContest } from "../../Providers/AuthProviders";
 import { useForm } from "react-hook-form";
 import toast, { Toaster } from 'react-hot-toast';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Register = () => {
   const { createUser, updateUserProfile, logOut } = useContext(AuthContest);
-  const [passwordError,setPasswordError] = useState("")
+  const [passwordError,setPasswordError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const {
     register,
@@ -55,7 +57,7 @@ const Register = () => {
   return (
     <div className="bg-base-200 py-3 lg:py-6 my-5">
       <p className="text-center pb-3 text-2xl text-blue-500 font-bold">Register Here</p>
-      <div className="rounded-xl mx-auto p-5 w-4/5 md:w-1/2 lg:w-1/3 bg-base-100">
+      <div className="rounded-xl mx-auto p-5 w-[300px] md:w-[450px] bg-base-100">
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="form-control">
             <label className="label">
@@ -97,17 +99,46 @@ const Register = () => {
             <label className="label">
               <span className="label-text text-lg text-orange-500 font-medium">Password</span>
             </label>
-            <input 
-            type="password" name="password" placeholder="Password" 
+            <div className="flex flex-col">
+              <input 
+                type={showPassword ? "text" : "password"} name="password" placeholder="Password" 
+                className="input input-bordered relative"
+                {...register("password", { required: true })} 
+              />
+              <span 
+                className="absolute translate-x-56 md:translate-x-96 translate-y-4" 
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                  {
+                    showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                  }
+              </span>
+              {errors.password && (
+                <span className="text-red-500">This field is required</span>
+              )}
+              {
+                passwordError && <small className='text-red-800'>{passwordError}</small>
+              }
+            </div>
+            {/* <input 
+            type={showPassword ? "text" : "password"} name="password" placeholder="Password" 
             className="input input-bordered"
             {...register("password", { required: true })} 
             />
+            <span 
+              className="absolute  center-5" 
+              onClick={() => setShowPassword(!showPassword)}
+            >
+                {
+                  showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                }
+            </span>
             {errors.password && (
               <span className="text-red-500">This field is required</span>
             )}
             {
               passwordError && <small className='text-red-800'>{passwordError}</small>
-            }
+            } */}
           </div>
           <div className="form-control mt-6">
             <button className="btn btn-outline border-2 text-orange-500 hover:bg-blue-500 hover:border-0">Register</button>
